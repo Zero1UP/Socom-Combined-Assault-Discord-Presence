c055bb73eebfa03e110e234ded445ba216b13e38
1UP <zero1up@gmail.com>
2020-01-01 20:18:16 -0500

    Initial code push

---
+++ Socom Combined Assault Discord Presence/Models/MapDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socom_Combined_Assault_Discord_Presence
+{
+    public class MapDataModel
+    {
+        public string _mapID;
+        public string _mapName;
+        public string _discordKey;
+
+        public MapDataModel(string mapID, string mapName, string discordID)
+        {
+            _mapID = mapID;
+            _mapName = mapName;
+            _discordKey = discordID;
+        }
+    }
+}
